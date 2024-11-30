a7e6c110c02ac8605dd4f0aead1482c0984bb9c3
ATGXicefires <atgxicefires666@gmail.com>
2024-11-30 23:41:17 +0800

    新增尋找圖片邏輯 可以搭配json使用 功能還不完善 但流程基本是對的 後續可以沿用此想法

--- test/json_test/test.py
+++ test/json_test/test.py
@@ -5,8 +5,9 @@ import json
 import time
 import cv2
 import pyautogui
+import threading
     
-def wait_10_until_image(image_path, confidence=0.9, timeout=10):
+def wait_until_image(image_path, confidence=0.9, timeout=10):
     '''
     在指定的超時時間內，檢查螢幕上是否出現與給定影像檔案相匹配的影像。
     
@@ -20,10 +21,19 @@ def wait_10_until_image(image_path, confidence=0.9, timeout=10):
     '''
     start_time = time.time()
     template = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    
     while time.time() - start_time < timeout:
-        result = pyautogui.locateOnScreen(template, confidence=confidence)
+        # 嘗試在螢幕上定位影像
+        result = pyautogui.locateOnScreen(template, confidence=confidence, grayscale=True)
+        # 如果找到匹配的影像，返回其位置
         if result is not None:
+            print(f"匹配成功，位置：{result}")
             return result
+        # 打印當前匹配的準確值
+        print(f"當前匹配準確值：{confidence}")
+        # 每秒檢測一次
+        time.sleep(1)
+    print("未找到匹配的影像")
     return None
 
 def load_json_variables(file_path):
@@ -49,9 +59,36 @@ def load_json_variables(file_path):
     return data
 
 if __name__ == "__main__":
+    Start = False
     # 導入 sv.json 的變數
-    json_variables = load_json_variables('test\json_test\sv.json')
+    json_variables = load_json_variables('test\\json_test\\sv.json')
     # 使用導入的變數
     print(json_variables)
-    main_ui.main()
     
+    # 在新執行緒中運行 main_ui.main()
+    ui_thread = threading.Thread(target=main_ui.main)
+    ui_thread.start()
+
+    while True:
+        # 重新載入 JSON 檔案以檢查 "Start" 的值
+        with open('test\\json_test\\sv.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        
+        # 檢查 "Start" 是否為 "1"
+        if data.get("Start") == "1":
+            print("Start")
+            Start = True
+            break
+        # 可以加入一些延遲以避免過於頻繁的檢查
+        time.sleep(1)
+    
+    if Start:
+        with open('test\\json_test\\sv.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        Target_img = data.get("Step_1")
+        location = wait_until_image(Target_img, confidence=0.9)
+        if location:
+            print("找到圖片")
+        else:
+            print("沒找到圖片")
+
